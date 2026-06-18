45fe108883f26d62bc96361650962b9f3eef646a
Developer1 <vijayendraraj.cloudops@gmail.com>
2026-06-18 20:07:41 +0000

    Initial Spring Boot application

---
+++ src/main/java/azure_devops_demo/AzureDevopsDemoApplication.java
@@ -0,0 +1,13 @@
+package azure_devops_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDevopsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDevopsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/azure_devops_demo/AzureDevopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package azure_devops_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureDevopsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
